536db2e8f20d555f49ac7e22ade3c86dcfb5ba98
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2025-01-16 10:05:55 +0000

    View/Urls setup - Task_mnagment app - Amend view to taeke unique user_id and amend url to pass this - amend link in base.html and add dtl tag

--- task_management/views.py
+++ task_management/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
-from .forms import TaskForm, EditTaskForm
+from .forms import TaskForm
 from .models import Task
 from django.urls import reverse_lazy, reverse
 # Create your views here.
 
 
-def task_dashboard(request):
+def task_dashboard(request, user_id):
     """
     Display the task dashboard.
 
@@ -18,24 +19,33 @@ def task_dashboard(request):
 
     :template:`task_management/task-dashboard.html`
     """
+    # Fetch the user by user_id
+    user = get_object_or_404(User, id=user_id)
 
-    return render(request, "task_management/task-dashboard.html")
+    # Add view data
 
-class AddTaskView(CreateView):
-    model = Task
-    form_class = TaskForm
-    template_name ='task_management/add_task.html'
-    success_url = reverse_lazy('home')
-    
-class UpdateTaskView(UpdateView):
-    model = Task
-    form_class = EditTaskForm
-    template_name = 'update-task.html'
-    success_url = reverse_lazy('home')
+    # Context to pass to the template
+    context = {
+        "user": user,
+    }
 
-class DeleteTaskView(DeleteView):
-    model = Task
-    template_name = 'delete-task.html'
-    success_url = reverse_lazy('home')
+    return render(request, "task_management/task-dashboard.html", context)
 
+# --------------------------------------------------------
 
+# class AddTaskView(CreateView):
+#     model = Task
+#     form_class = TaskForm
+#     template_name ='task_management/add_task.html'
+#     success_url = reverse_lazy('home')
+
+# class UpdateTaskView(UpdateView):
+#     model = Task
+#     form_class = EditTaskForm
+#     template_name = 'update-task.html'
+#     success_url = reverse_lazy('home')
+
+# class DeleteTaskView(DeleteView):
+#     model = Task
+#     template_name = 'delete-task.html'
+#     success_url = reverse_lazy('home')
