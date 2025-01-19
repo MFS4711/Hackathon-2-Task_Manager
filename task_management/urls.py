01206ca2ea5107a42f2d4d5c4098b0ebcdb731d7
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2025-01-19 12:41:28 +0000

    Code Review - clean up of files and validation - add results to readme

--- task_management/urls.py
+++ task_management/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import task_dashboard, task_edit, task_delete, task_add
 from . import views
 
 urlpatterns = [
     path('task-dashboard/<int:user_id>/',
-         task_dashboard, name='task_dashboard'),
-    path('task-edit/<int:user_id>/<int:task_id>/', task_edit, name='task_edit'),
+         views.task_dashboard, name='task_dashboard'),
+    path('task-edit/<int:user_id>/<int:task_id>/', views.task_edit, name='task_edit'),
     path('task-delete/<int:user_id>/<int:task_id>/',
-         task_delete, name='task_delete'),
-    path('task-add/<int:user_id>/', task_add, name='task_add'),
+         views.task_delete, name='task_delete'),
+    path('task-add/<int:user_id>/', views.task_add, name='task_add'),
 ]
